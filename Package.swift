97e1006646d0a715af1d92506a156249593d7190
Albert Aleksieiev <albert.aleksieiev@gmail.com>
2018-08-31 17:03:57 +0300

    Dev: fix android compilation

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .executable(name: "NIOTLSServer", targets: ["NIOTLSServer"]),
     ],
     dependencies: [
-    .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+    .package(url: "https://github.com/readdle/swift-nio", .exact("1.9.3")),
     .package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"),
     ],
     targets: [
